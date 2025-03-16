3482faafe166b6fc964fe661ba63e95610e1f646
Cytex124 <andreas.wyrobek95@gmail.com>
2025-03-16 21:30:56 +0100

    Adjust spacing utility in Logo component
    Replaced `gap-0.5` with `gap-px` for more precise control over spacing in the Logo component. This ensures a finer and more consistent layout alignment.

--- src/components/Logo.tsx
+++ src/components/Logo.tsx
@@ -1,6 +1,6 @@
 export function Logo() {
 	return (
-		<div className="flex items-center gap-0.5 ">
+		<div className="flex items-center gap-px ">
 			<span className="font-extrabold! text-2xl! text-white!">Wyro</span>
 			<span className="font-thin! text-2xl! text-white!">Tech</span>
 		</div>
