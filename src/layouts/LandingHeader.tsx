e8fc9fae728e2a0053b20ecb0ef617b3d9bd9bc9
Cytex124 <andreas.wyrobek95@gmail.com>
2025-03-16 21:49:19 +0100

    Update LandingHeader layout and adjust styling
    Refactored the layout to wrap content in a full-width section container and center it using `mx-auto`. Adjusted padding for improved responsiveness and aligned elements more effectively on larger screens with updated spacing.

--- src/layouts/LandingHeader.tsx
+++ src/layouts/LandingHeader.tsx
@@ -2,17 +2,19 @@ import Button from "@/components/Button.tsx";
 
 export function LandingHeader() {
 	return (
-		<section className="flex flex-col justify-center lg:justify-start items-center gap-12 px-8 py-16 max-w-2xl h-screen lg:pt-[33%]">
-			<div className="flex flex-col gap-6 items-center">
-				<h1 className="text-center">Lorem ipsum dolor</h1>
-				<h5 className="text-center">
-					Lorem ipsum dolor sit amet, consetetur sadipscing elitr, sed diam
-					nonumy eirmod tempor invidunt ut labore et dolore magna aliquyam.
-				</h5>
-			</div>
-			<div className="flex gap-4">
-				<Button severity="secondary" text="My Work" icon="chevron-right" />
-				<Button severity="primary" text="Contact Us" icon="chevron-right" />
+		<section className="w-screen">
+			<div className="flex flex-col justify-center lg:justify-start items-center gap-12 px-8 py-16 max-w-2xl h-screen lg:pt-[25%] mx-auto">
+				<div className="flex flex-col gap-6 items-center">
+					<h1 className="text-center">Lorem ipsum dolor</h1>
+					<h5 className="text-center">
+						Lorem ipsum dolor sit amet, consetetur sadipscing elitr, sed diam
+						nonumy eirmod tempor invidunt ut labore et dolore magna aliquyam.
+					</h5>
+				</div>
+				<div className="flex gap-4">
+					<Button severity="secondary" text="My Work" icon="chevron-right" />
+					<Button severity="primary" text="Contact Us" icon="chevron-right" />
+				</div>
 			</div>
 		</section>
 	);
