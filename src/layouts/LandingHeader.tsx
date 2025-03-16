dce3e38e701dcb39ce9a74f832ca06ac1cad5f73
Cytex124 <andreas.wyrobek95@gmail.com>
2025-03-16 21:47:46 +0100

    Adjust LandingHeader layout for better responsiveness
    Added 'justify-center' and 'lg:justify-start' to improve vertical alignment across different screen sizes. Made the top padding responsive by applying 'lg:pt-[33%]' only on large screens. This ensures a consistent and optimized user experience on various devices.

--- src/layouts/LandingHeader.tsx
+++ src/layouts/LandingHeader.tsx
@@ -2,7 +2,7 @@ import Button from "@/components/Button.tsx";
 
 export function LandingHeader() {
 	return (
-		<section className="flex flex-col items-center gap-12 px-8 py-16 max-w-2xl h-screen pt-[33%]">
+		<section className="flex flex-col justify-center lg:justify-start items-center gap-12 px-8 py-16 max-w-2xl h-screen lg:pt-[33%]">
 			<div className="flex flex-col gap-6 items-center">
 				<h1 className="text-center">Lorem ipsum dolor</h1>
 				<h5 className="text-center">
