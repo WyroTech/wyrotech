1d33d0e941bf76b4e323430719785f516979fe0b
Cytex124 <andreas.wyrobek95@gmail.com>
2025-03-16 21:42:19 +0100

    Add LandingHeader component and integrate into homepage
    Introduced a new LandingHeader component to enhance the homepage layout. Removed unnecessary padding in the main layout for better spacing. The LandingHeader includes a hero section with buttons for navigation and is loaded as a React client-only component.

---
+++ src/layouts/LandingHeader.tsx
@@ -0,0 +1,16 @@
+import Button from "@/components/Button.tsx";
+
+export function LandingHeader() {
+	return (
+		<section className="flex flex-col justify-center items-center gap-8 px-8 py-16 max-w-2xl h-screen">
+			<div className="flex flex-col gap-4 items-center">
+				<h1>Lorem ipsum</h1>
+				<h4>Lorem ipsum</h4>
+			</div>
+			<div className="flex gap-4">
+				<Button severity="secondary" text="My Work" icon="chevron-right" />
+				<Button severity="primary" text="Contact Us" icon="chevron-right" />
+			</div>
+		</section>
+	);
+}
