184b704f5580389f04901b41aef5eccc8698c39c
Cytex124 <andreas.wyrobek95@gmail.com>
2025-03-16 21:43:55 +0100

    Adjust LandingHeader vertical alignment with padding tweak
    Replaces `justify-center` with `pt-[33%]` to refine vertical alignment. This ensures more precise control over spacing and layout consistency for the header section.

--- src/layouts/LandingHeader.tsx
+++ src/layouts/LandingHeader.tsx
@@ -2,7 +2,7 @@ import Button from "@/components/Button.tsx";
 
 export function LandingHeader() {
 	return (
-		<section className="flex flex-col justify-center items-center gap-8 px-8 py-16 max-w-2xl h-screen">
+		<section className="flex flex-col items-center gap-8 px-8 py-16 max-w-2xl h-screen pt-[33%]">
 			<div className="flex flex-col gap-4 items-center">
 				<h1>Lorem ipsum</h1>
 				<h4>Lorem ipsum</h4>
