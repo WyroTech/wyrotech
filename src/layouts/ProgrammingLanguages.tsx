b2de246edac1e887335f73b249615f20c99641db
Cytex124 <andreas.wyrobek95@gmail.com>
2025-03-19 00:13:16 +0100

    Update programming icons layout and include new dependencies
    Revised the `ProgrammingIcon` layout for better design and added new technologies such as Azure, dotnet, and Sass. Switched from `bxl` to `logos` icon set for improved clarity and included `@iconify-json/logos` as a dependency. Adjusted styling and spacing for a more cohesive appearance.

--- src/layouts/ProgrammingLanguages.tsx
+++ src/layouts/ProgrammingLanguages.tsx
@@ -1,10 +1,12 @@
 import {Icon} from "@iconify/react";
 
 function ProgrammingIcon({ icon, name }: { icon: string; name: string }) {
-	const size = 96;
+	const size = 72;
 	return (
-		<div className="opacity-50 hover:opacity-100 transition-opacity cube flex flex-col items-center gap-1">
-			<Icon icon={`bxl:${icon}`} height={size} />
+		<div className="transition-opacity cube group-hover:opacity-40 hover:!opacity-100 cube flex flex-col items-center gap-0.5">
+			<div className="w-24 h-24 flex justify-center items-center">
+				<Icon icon={`logos:${icon}`} width={size} height={size} />
+			</div>
 			<span className="w-full text-center text-sm!">{name}</span>
 		</div>
 	);
@@ -13,19 +15,28 @@ function ProgrammingIcon({ icon, name }: { icon: string; name: string }) {
 export function ProgrammingLanguages() {
 	const size = 96;
 	return (
-		<div className="flex flex-wrap w-full justify-center gap-x-4 gap-y-8 py-16 px-8">
-			<ProgrammingIcon name="Docker" icon="docker" />
-			<ProgrammingIcon name="Tailwind" icon="tailwind-css" />
-			<ProgrammingIcon name="Python" icon="python" />
-			<ProgrammingIcon name="HTML5" icon="html5" />
-			<ProgrammingIcon name="CSS3" icon="css3" />
-			<ProgrammingIcon name="Javascript" icon="javascript" />
-			<ProgrammingIcon name="Typescript" icon="typescript" />
-			<ProgrammingIcon name="Angular" icon="angular" />
-			<ProgrammingIcon name="React" icon="react" />
-			<ProgrammingIcon name="Linux" icon="tux" />
-			<ProgrammingIcon name="Git" icon="git" />
-			<ProgrammingIcon name="Figma" icon="figma" />
+		<div className="py-24 px-8">
+			<div className="group flex flex-wrap w-full justify-center gap-x-12 gap-y-16">
+				<ProgrammingIcon name="Docker" icon="docker-icon" />
+				<ProgrammingIcon name="Tailwind" icon="tailwindcss-icon" />
+				<ProgrammingIcon name="Python" icon="python" />
+				<ProgrammingIcon name="HTML5" icon="html-5" />
+				<ProgrammingIcon name="CSS3" icon="css-3" />
+				<ProgrammingIcon name="Javascript" icon="javascript" />
+				<ProgrammingIcon name="Typescript" icon="typescript-icon" />
+				<ProgrammingIcon name="Angular" icon="angular-icon" />
+				<ProgrammingIcon name="RxJs" icon="reactivex" />
+				<ProgrammingIcon name="React" icon="react" />
+				<ProgrammingIcon name="Linux" icon="tux" />
+				<ProgrammingIcon name="Git" icon="git-icon" />
+				<ProgrammingIcon name="Figma" icon="figma" />
+				<ProgrammingIcon name="Azure" icon="azure-icon" />
+				<ProgrammingIcon name="dotnet" icon="dotnet" />
+				<ProgrammingIcon name="C#" icon="c-sharp" />
+				<ProgrammingIcon name="Sass" icon="sass" />
+				<ProgrammingIcon name="PostgreSQL" icon="postgresql" />
+				<ProgrammingIcon name="Posthog" icon="posthog-icon" />
+			</div>
 		</div>
 	);
 }
