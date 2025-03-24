2c22d6b0bf5abdb9f53297d7af9c0e8126a7d63a
Cytex124 <andreas.wyrobek95@gmail.com>
2025-03-24 23:47:54 +0100

    Refactor footer layout and optimize logo image handling for improved performance

--- src/components/Logo.tsx
+++ src/components/Logo.tsx
@@ -1,9 +1,7 @@
 export function Logo(props: { imageUrl: string }) {
 	return (
 		<div className="flex items-center gap-px">
-			<div className="mr-1">
-				<img src={props.imageUrl} alt="logo" width="28" />
-			</div>
+			<img src={props.imageUrl} alt="logo" width="28" className="mr-1" />
 			<span className="font-extrabold! text-2xl! text-white!">Wyro</span>
 			<span className="font-thin! text-2xl! text-white!">Tech</span>
 		</div>
