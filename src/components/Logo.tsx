9451003f66b695287533019b035181cc2e4e052c
Cytex124 <andreas.wyrobek95@gmail.com>
2025-03-25 20:57:10 +0100

    Add font preconnect links and update logo image handling for improved performance

--- src/components/Logo.tsx
+++ src/components/Logo.tsx
@@ -1,7 +1,13 @@
 export function Logo(props: { imageUrl: string }) {
 	return (
 		<div className="flex items-center gap-px">
-			<img src={props.imageUrl} alt="logo" width="28" className="mr-1" />
+			<img
+				src={props.imageUrl}
+				alt="logo"
+				width="28"
+				height="28"
+				className="mr-1"
+			/>
 			<span className="font-extrabold! text-2xl! text-white!">Wyro</span>
 			<span className="font-thin! text-2xl! text-white!">Tech</span>
 		</div>
