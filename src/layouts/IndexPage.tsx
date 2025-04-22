1ff7c030bc313a740b6b83702671754ff5a794ee
wyrotech <andreas.wyrobek95@gmail.com>
2025-04-22 16:12:48 +0200

    Simplify IndexPage layout by removing unused components
    Removed unused components such as ProgrammingLanguages, SpeakingLanguages, Stats, Projects, and WorkTimeline from the IndexPage. This cleanup reduces complexity and ensures a more focused layout for the page. Other essential components remain intact to preserve functionality.

--- src/layouts/IndexPage.tsx
+++ src/layouts/IndexPage.tsx
@@ -1,23 +1,13 @@
 import {Navigation} from "@/components/Navigation.tsx";
 import {Footer} from "@/layouts/Footer.tsx";
-import {ProgrammingLanguages} from "@/layouts/ProgrammingLanguages.tsx";
-import {Projects} from "@/layouts/Projects.tsx";
-import {SpeakingLanguages} from "@/layouts/SpeakingLanguages.tsx";
-import {WorkTimeline} from "@/layouts/WorkTimeline.tsx";
 import type {LandingPageObject} from "@/lib/models.ts";
 import {LandingHeader} from "./LandingHeader";
-import {Stats} from "./Stats";
 
 export function IndexPage(props: { obj: LandingPageObject }) {
 	return (
 		<>
 			<Navigation logo={props.obj.logoSmall} />
 			<LandingHeader profileUrl={props.obj.profileUrl} />
-			<ProgrammingLanguages />
-			<SpeakingLanguages />
-			<Stats />
-			<Projects projects={props.obj.projects} />
-			<WorkTimeline images={props.obj.timelineImages} />
 			<Footer logoUrl={props.obj.logoSmall} />
 		</>
 	);
