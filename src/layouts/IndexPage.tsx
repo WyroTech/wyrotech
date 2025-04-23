b8ca30fdd710fa9afa1a23c17c3a35e9d4134785
Cytex124 <andreas.wyrobek95@gmail.com>
2025-04-23 23:54:00 +0200

    fix loading

--- src/layouts/IndexPage.tsx
+++ src/layouts/IndexPage.tsx
@@ -5,22 +5,31 @@ import {Projects} from "@/layouts/Projects.tsx";
 import {SpeakingLanguages} from "@/layouts/SpeakingLanguages.tsx";
 import {WorkTimeline} from "@/layouts/WorkTimeline.tsx";
 import type {LandingPageObject} from "@/lib/models.ts";
+import {useState} from "react";
 import {LandingHeader} from "./LandingHeader";
 import {Stats} from "./Stats";
 
 export function IndexPage(props: { obj: LandingPageObject }) {
-	setTimeout(() => (window as any).removeLoadingScreen(), 0);
+	const [isLoading, setIsLoading] = useState(true);
+	setTimeout(() => {
+		setTimeout(() => setIsLoading(false), 500);
+		(window as any).removeLoadingScreen();
+	}, 0);
 
 	return (
 		<>
-			<Navigation logo={props.obj.logoSmall} />
-			<LandingHeader profileUrl={props.obj.profileUrl} />
-			<ProgrammingLanguages />
-			<SpeakingLanguages />
-			<Stats />
-			<Projects projects={props.obj.projects} />
-			<WorkTimeline images={props.obj.timelineImages} />
-			<Footer logoUrl={props.obj.logoSmall} />
+			{!isLoading && (
+				<>
+					<Navigation logo={props.obj.logoSmall} />
+					<LandingHeader profileUrl={props.obj.profileUrl} />
+					<ProgrammingLanguages />
+					<SpeakingLanguages />
+					<Stats />
+					<Projects projects={props.obj.projects} />
+					<WorkTimeline images={props.obj.timelineImages} />
+					<Footer logoUrl={props.obj.logoSmall} />
+				</>
+			)}
 		</>
 	);
 }
