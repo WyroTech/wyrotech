51a1f8516ba6a9bf1a6c33ea2d0d94144c998055
Cytex124 <andreas.wyrobek95@gmail.com>
2025-04-25 11:25:57 +0200

    Refactor layouts and optimize language components
    Reorganized layouts by migrating `LandingHeader`, `ProgrammingLanguages`, and `SpeakingLanguages` to `.astro` files for better separation of concerns. Removed unused animations and streamlined components for improved performance and maintainability. Added `og:site_name` metadata to enhance SEO.

--- src/layouts/IndexPage.tsx
+++ src/layouts/IndexPage.tsx
@@ -1,12 +1,9 @@
 import {Navigation} from "@/components/Navigation.tsx";
 import {Footer} from "@/layouts/Footer.tsx";
-import {ProgrammingLanguages} from "@/layouts/ProgrammingLanguages.tsx";
 import {Projects} from "@/layouts/Projects.tsx";
-import {SpeakingLanguages} from "@/layouts/SpeakingLanguages.tsx";
 import {WorkTimeline} from "@/layouts/WorkTimeline.tsx";
 import type {LandingPageObject} from "@/lib/models.ts";
 import {useEffect, useState} from "react";
-import {LandingHeader} from "./LandingHeader";
 import {Stats} from "./Stats";
 
 export function IndexPage(props: { obj: LandingPageObject }) {
@@ -34,9 +31,6 @@ export function IndexPage(props: { obj: LandingPageObject }) {
 			{!isLoading && (
 				<>
 					<Navigation logo={props.obj.logoSmall} />
-					<LandingHeader profileUrl={props.obj.profileUrl} />
-					<ProgrammingLanguages />
-					<SpeakingLanguages />
 					<Stats />
 					<Projects projects={props.obj.projects} />
 					<WorkTimeline images={props.obj.timelineImages} />
