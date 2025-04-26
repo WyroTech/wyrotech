ab528b6ed11abeaec2cb3b0e7131445cc07cf43b
Cytex124 <andreas.wyrobek95@gmail.com>
2025-04-26 19:49:45 +0200

    Refactor Projects component into Astro file and update usage.
    Extracted the Projects component from `IndexPage.tsx` to a standalone `Projects.astro` file. Updated the layout to utilize the new component, ensuring proper props are passed and maintaining consistent functionality. This improves separation of concerns and enhances readability.

--- src/layouts/IndexPage.tsx
+++ src/layouts/IndexPage.tsx
@@ -1,4 +1,3 @@
-import {Projects} from "@/layouts/Projects.tsx";
 import {WorkTimeline} from "@/layouts/WorkTimeline.tsx";
 import type {LandingPageObject} from "@/lib/models.ts";
 import {Stats} from "./Stats";
@@ -7,7 +6,6 @@ export function IndexPage(props: { obj: LandingPageObject }) {
 	return (
 		<>
 			<Stats />
-			<Projects projects={props.obj.projects} />
 			<WorkTimeline images={props.obj.timelineImages} />
 		</>
 	);
