5af00bf70b895142da6af507d28bfa5fe067402b
Cytex124 <andreas.wyrobek95@gmail.com>
2025-04-26 02:10:01 +0200

    Refactor text alignment in LandingHeader for better UX.
    Aligned the `h1` element to center on smaller screens and right-align on larger screens, improving consistency across devices. This adjustment enhances the visual appeal and clarity of

--- src/layouts/LandingHeader.tsx
+++ src/layouts/LandingHeader.tsx
@@ -50,7 +50,7 @@ export function LandingHeader({
 
 				<div className="flex flex-col gap-12 items-center lg:items-end w-full lg:w-1/2">
 					<div className="flex flex-col items-center lg:items-end">
-						<h1 className="text-7xl! lg:text-[9vw]! relative z-10 pt-12 leading-[.9]">
+						<h1 className="text-center lg:text-right text-7xl! lg:text-[9vw]! relative z-10 pt-12 leading-[.9]">
 							Andreas
 							<br />
 							<div className="text-gradiant">Wyrobek</div>
