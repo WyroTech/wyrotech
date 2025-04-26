62a37bc9e755bdce67774472a09a4aaf08ad3878
Cytex124 <andreas.wyrobek95@gmail.com>
2025-04-26 19:46:27 +0200

    Refactor and migrate SpeakingLanguages component
    Removed SpeakingLanguages from IndexPage.tsx and refactored it to use a new Astro component. This simplifies the implementation by removing unnecessary dependencies and improving maintainability. Updated the English index page to

--- src/layouts/IndexPage.tsx
+++ src/layouts/IndexPage.tsx
@@ -1,5 +1,4 @@
 import {Projects} from "@/layouts/Projects.tsx";
-import {SpeakingLanguages} from "@/layouts/SpeakingLanguages.tsx";
 import {WorkTimeline} from "@/layouts/WorkTimeline.tsx";
 import type {LandingPageObject} from "@/lib/models.ts";
 import {Stats} from "./Stats";
@@ -7,7 +6,6 @@ import {Stats} from "./Stats";
 export function IndexPage(props: { obj: LandingPageObject }) {
 	return (
 		<>
-			<SpeakingLanguages />
 			<Stats />
 			<Projects projects={props.obj.projects} />
 			<WorkTimeline images={props.obj.timelineImages} />
--- src/layouts/SpeakingLanguages.tsx
+++ src/layouts/SpeakingLanguages.tsx
@@ -1,7 +1,5 @@
 import {getLangFromWindow, useTranslations} from "@/i18n/utils.ts";
 import {Icon} from "@iconify/react";
-import {motion, useInView} from "framer-motion";
-import {useRef} from "react";
 
 const lang = getLangFromWindow();
 const t = useTranslations(lang);
@@ -13,7 +11,7 @@ function LanguageIcon({
 }: { icon: string; name: string; subText: string }) {
 	const size = 72;
 	return (
-		<motion.div className="transition-all cube hover:!scale-120 cube flex flex-col items-center gap-0.5 max-w-24">
+		<div className="transition-all cube hover:!scale-120 cube flex flex-col items-center gap-0.5 max-w-24">
 			<div className="w-24 h-24 flex justify-center items-center">
 				<Icon icon={`flag:${icon}`} width={size} height={size} />
 			</div>
@@ -21,33 +19,22 @@ function LanguageIcon({
 			<span className="w-full text-center text-slate-400! text-xs!">
 				{subText}
 			</span>
-		</motion.div>
+		</div>
 	);
 }
 
 export function SpeakingLanguages() {
-	const ref = useRef(null);
-	const isInView = useInView(ref, { once: true, amount: 0.1 });
-
 	return (
-		<motion.div
-			className="min-h-[100lvh] flex flex-col items-center justify-center px-8 gap-16"
-			initial={{ opacity: 0, y: -40 }}
-			animate={isInView ? { opacity: 1, y: 0 } : {}}
-			transition={{ duration: 0.5 }}
-		>
+		<div className="min-h-[100lvh] flex flex-col items-center justify-center px-8 gap-16">
 			<div className="relative pt-8 z-10">
 				<h1 className="relative title z-20">{t("title.languagesIspeak")}</h1>
 			</div>
-			<div
-				ref={ref}
-				className="group grid grid-cols-2 sm:flex sm:flex-wrap w-full justify-center justify-items-center gap-x-12 gap-y-16 pb-16 z-20"
-			>
+			<div className="group grid grid-cols-2 sm:flex sm:flex-wrap w-full justify-center justify-items-center gap-x-12 gap-y-16 pb-16 z-20">
 				<LanguageIcon name="Deutsch" subText="Native" icon="de-4x3" />
 				<LanguageIcon name="Polski" subText="2nd Native" icon="pl-4x3" />
 				<LanguageIcon name="English" subText="C1 Level" icon="gb-4x3" />
 				<LanguageIcon name="日本語" subText="N5 Level" icon="jp-4x3" />
 			</div>
-		</motion.div>
+		</div>
 	);
 }
