64dd78a1775214fea9d9af97cc571d71387b2282
Cytex124 <andreas.wyrobek95@gmail.com>
2025-04-26 18:56:42 +0200

    Refactor layout components and enhance footer styling
    Extracted `Footer` and `AnimatedLink` into separate components to improve modularity. Updated `IconLink` for better accessibility and styling. Adjusted `Logo` component to support dynamic sizing and styles. Integrated new components into pages for a cleaner layout structure.

--- src/layouts/IndexPage.tsx
+++ src/layouts/IndexPage.tsx
@@ -1,5 +1,3 @@
-import {Navigation} from "@/components/Navigation.tsx";
-import {Footer} from "@/layouts/Footer.tsx";
 import {ProgrammingLanguages} from "@/layouts/ProgrammingLanguages.tsx";
 import {Projects} from "@/layouts/Projects.tsx";
 import {SpeakingLanguages} from "@/layouts/SpeakingLanguages.tsx";
@@ -10,13 +8,11 @@ import {Stats} from "./Stats";
 export function IndexPage(props: { obj: LandingPageObject }) {
 	return (
 		<>
-			<Navigation logo={props.obj.logoSmall} />
 			<ProgrammingLanguages />
 			<SpeakingLanguages />
 			<Stats />
 			<Projects projects={props.obj.projects} />
 			<WorkTimeline images={props.obj.timelineImages} />
-			<Footer logoUrl={props.obj.logoSmall} />
 		</>
 	);
 }
